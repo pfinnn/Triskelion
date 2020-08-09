e9a5d16c48c5d6999f6739bae8ee8ee14db04258
pfinnn <finn.isengardt@googlemail.com>
2020-08-09 19:25:10 +0200

    Typo Fix Class Name, Debug Deal Damage

--- Assets/Scripts/Abstract/Damagable.cs
+++ Assets/Scripts/Abstract/Damagable.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Damagable : MonoBehaviour
-{
-    public int maxHealth;
+public abstract class Damageable : MonoBehaviour
+{   
+    [SerializeField]
+    private int maxHealth;
     private int health;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         health = maxHealth;
     }
 
-    public void dealDemage(int amount)
+    public void dealDamage(int amount)
     {
         health -= amount;
     }
@@ -25,5 +26,10 @@ public abstract class Damagable : MonoBehaviour
     public int getMaxHealth()
     {
         return maxHealth;
+    }
+
+    public void setHealth(int _health)
+    {
+        health = _health;
     }
 }
--- Assets/Scripts/Entities/Triskelion.cs
+++ Assets/Scripts/Entities/Triskelion.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Triskelion : Damagable
+public class Triskelion : Damageable
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.getHealth()==0)
+        // catch button down to debug damage taken
+
+        if (Input.GetKeyDown(KeyCode.Backspace)){
+            dealDamage(10);
+            // play "damage taken animation"
+
+            // notify UI Manager, should not be handled here
+        }
+
+        if (this.getHealth()==0)
         {
             //Game Over Event
         }
