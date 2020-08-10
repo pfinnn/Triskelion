c2ec68c16f1df0cfdc033debf97482bc0d981c7f
pfinnn <finn.isengardt@googlemail.com>
2020-08-10 17:33:07 +0200

    Adding Enemy Class with State Enum

---
+++ Assets/Scripts/Entities/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Damageable
+{
+
+    enum State
+    {
+        Attacking,
+        Moving,
+        Dying
+    }
+
+    State currentState = State.Attacking;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead())
+        {
+            // play death animation
+            // play death sound
+            // notify wave manager, handles destruction
+
+            
+        }
+    }
+
+
+}
