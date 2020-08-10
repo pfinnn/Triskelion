9a9dc4242a3b42d20bbabfa47c802d02d853f637
Vincent Kinder <plaron@fn.de>
2020-08-10 19:39:44 +0200

    Added Tower Prefab

---
+++ Assets/Scripts/Entities/Tower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : Damageable
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            DealDamage(10);
+            if (GetHealth() <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+
+    }
+}
