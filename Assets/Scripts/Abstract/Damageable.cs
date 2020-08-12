cae1e7667502514449345a4d2ccdb4eb6f14d09e
Vincent Kinder <plaron@fn.de>
2020-08-12 16:43:05 +0200

    Damageable shouldnt be removed

---
+++ Assets/Scripts/Abstract/Damageable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Damageable : MonoBehaviour
+{   
+    [SerializeField]
+    private int maxHealth = 100;
+    private int health;
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void DealDamage(int amount)
+    {
+        health -= amount;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void SetHealth(int _health)
+    {
+        health = _health;
+    }
+
+    public bool isDead()
+    {
+        return (health <= 0);
+    }
+
+    public void OnDestroyedEvent()
+    {
+        throw new NotImplementedException();
+    }
+}
