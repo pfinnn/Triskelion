9d99ac3ab749d742c5d97be1e747374559d239b1
pfinnn <finn.isengardt@googlemail.com>
2020-08-13 15:23:40 +0200

    Unit Controller relative positions, spawning in formation, delegating movement to agents

---
+++ Assets/Scripts/Systems/Navigation/UnitController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;    
+    
+    Vector3 targetPosition;
+
+    //List<Formations> formations;
+
+    [SerializeField]
+    int soldiersAmount;
+
+    [SerializeField]
+    int soldiersPerRow;
+
+    [SerializeField]
+    GameObject unitPrefab; // TODO, use different start animatin states on spawn, dont use sperate prefabs you maniac
+
+    List<GameObject> soldiers;
+
+    private void Awake()
+    {
+        if (target == null)
+        {
+            targetPosition = this.transform.position;
+        }
+        else
+        {
+            targetPosition = target.position;
+        }
+
+        soldiers = new List<GameObject>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnUnitsInFormation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateNewTargetPosition();
+    }
+
+    void SpawnUnitsInFormation()
+    {
+        Vector3 center = this.transform.position;
+
+        int rows = Mathf.RoundToInt(soldiersAmount / soldiersPerRow);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < soldiersPerRow; j++)
+            {
+                Vector3 soldierPos = center + new Vector3(i, 0, j);
+                GameObject soldier = Instantiate(unitPrefab, soldierPos, transform.rotation);
+                soldier.transform.SetParent(this.transform);
+                soldiers.Add(soldier);
+            }
+        }
+    }
+
+    internal Vector3 CalculateCenter()
+    {
+        Vector3 total = Vector3.zero;
+        foreach (GameObject s in soldiers)
+        {
+            total +=  s.transform.position;
+        }
+
+        return total/soldiers.Count;
+    }
+
+    internal void CalculateNewTargetPosition()
+    {
+        Vector3 center = CalculateCenter();
+
+        for (int i = 0; i < soldiersAmount; i++)
+        {
+            Vector3 relativeToCenter = soldiers[i].GetComponent<AgentMovement>().GetCurrentPosition() - center;
+            Vector3 agentDirection = targetPosition + relativeToCenter;
+            Debug.DrawLine(soldiers[i].transform.position, agentDirection);
+            soldiers[i].GetComponent<AgentMovement>().SetTargetDestination(agentDirection);
+        }
+
+    }
+
+    internal void SetTarget(Vector3 _targetPosition)
+    {
+        targetPosition = _targetPosition;
+    }
+
+}
