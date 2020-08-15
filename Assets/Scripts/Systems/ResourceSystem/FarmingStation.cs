c9d4b51357936e0417d2ed76eae7f125b20133ea
Vincent Kinder <plaron@fn.de>
2020-08-15 23:32:49 +0200

    BaseClass FarmingStation for Resource Gathering

---
+++ Assets/Scripts/Systems/ResourceSystem/FarmingStation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmingStation : MonoBehaviour
+{
+    [SerializeField]
+    private float addingIntervallInSeconds;
+    [SerializeField]
+    private Inventory.resourceType resourceType;
+    [SerializeField]
+    private Inventory resourceSystem;
+
+    private float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= addingIntervallInSeconds)
+        {
+            resourceSystem.AddResource(resourceType, 1);
+            timer = 0f;
+        }
+    }
+}
