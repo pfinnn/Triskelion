503101c621ee9826eede264aeb14e957e2f77488
pfinnn <finn.isengardt@googlemail.com>
2020-08-19 19:51:07 +0200

    ShootingSystem now can shoot SoldierInUnit, shorten function names

--- Assets/Scripts/Systems/BattleSystem/ShootingSystem.cs
+++ Assets/Scripts/Systems/BattleSystem/ShootingSystem.cs
@@ -1,32 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShootingSystem : MonoBehaviour
 {
     [SerializeField]
     private GameObject projectile;
 
+    private Vector3 shootingPoint;
+
+    Vector3 DEBUG_ORIGIN;
+    Vector3 DEBUG_TARGET;
+
+    private void Start()
+    {
+        shootingPoint = new Vector3(this.transform.position.x, this.transform.position.y+6, this.transform.position.z);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawLine(DEBUG_ORIGIN, DEBUG_TARGET, Color.red);
     }
 
-    private Vector3 LocateEstimatedPositionOfTargetWhileMoving(Transform targetPosition, float targetSpeed)
-    {
-        return Vector3.MoveTowards(targetPosition.position, targetPosition.position, 10f);
-        //return targetPosition.position + (targetPosition.forward.normalized * targetSpeed);
+    internal void ShootSoldierInUnit(Transform soldier, float soldierSpeed)
+    {        
+        DEBUG_ORIGIN = shootingPoint;
+        DEBUG_TARGET = soldier.position;
+
+        soldier.GetComponent<Soldier>().DealDamage(100);
     }
 
-    public void LaunchProjectileWithArcMovingTarget(Transform target, float targetSpeed)
+    private Vector3 EstimatePosMovingTarget(Transform target, float targetSpeed)
     {
-        Vector3 Vo = CalculateVelocity(LocateEstimatedPositionOfTargetWhileMoving(target, targetSpeed), transform.position, 2f);
+        //return Vector3.MoveTowards(targetPosition.position, targetPosition.position, 10f);
+        return target.position + (target.forward.normalized * targetSpeed);
+    }
+
+    internal void ShootArcMovingTarget(Transform target, float targetSpeed)
+    {
+        Vector3 Vo = CalculateVelocity(EstimatePosMovingTarget(target, targetSpeed), transform.position, 2f);
         Vector3 shootingPoint = Vector3.MoveTowards(transform.position, target.position, 5f);
         GameObject obj = Instantiate(projectile, shootingPoint, Quaternion.identity);
         obj.GetComponent<Rigidbody>().velocity = Vo;
     }
 
-    public void LaunchProjectileWithArcStaticTarget(Transform origin, Transform target)
+    internal void ShootArcStaticTarget(Transform origin, Transform target)
     {
         Vector3 Vo = CalculateVelocity(target.position, origin.position, 5f);
         Vector3 shootingPoint = origin.position + new Vector3 (0,1,0);
