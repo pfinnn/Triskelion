d47c488a75d10057fcd63c7ce042a39db12d2bf7
pfinnn <finn.isengardt@googlemail.com>
2020-08-22 18:23:57 +0200

    Adding Player Manager to handle Player States, right click to go back to default state

---
+++ Assets/Scripts/Game Managers/PlayerManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    SpellManager spm;
+
+    public enum State_Player
+    {
+        Default,
+        CastingSpell,
+    }
+
+    public State_Player currentState = State_Player.Default;
+
+    private void Awake()
+    {
+        spm = GetComponentInParent<SpellManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            currentState = State_Player.Default;
+        }
+
+        switch (currentState)
+        {
+            case State_Player.Default:
+                break;
+
+            case State_Player.CastingSpell:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //int layerMask = 5;
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit, 1000))
+                    {
+                        spm.CastSpellOnPosition(hit.point);
+                    }
+                    if (Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("UI")))
+                    {
+                        
+                    }
+
+
+                }
+                break;
+        }
+
+    }
+
+    public void ChangeState(State_Player state)
+    {
+        switch (state)
+        {
+            case State_Player.Default:
+                currentState = State_Player.Default;
+                break;
+            case State_Player.CastingSpell:
+                currentState = State_Player.CastingSpell;
+                break;
+        }
+    }
+}
