58d68be3f87223d0fd6329dea20503e3b2f9a34a
pfinnn <finn.isengardt@googlemail.com>
2020-08-23 17:32:04 +0200

    Update Worker.cs

--- Assets/Scripts/Entities/Worker.cs
+++ Assets/Scripts/Entities/Worker.cs
@@ -29,40 +29,6 @@ public class Worker : Damageable
     [SerializeField]
     private State currentState = State.WAITING;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //switch (currentState)
-        //{
-            //case State.COLLECTING_RESOURCE:
-            //    CollectResource();
-            //    break;
-
-            //case State.MOVING_WORKPLACE:
-            //    //agent.SetTargetDestination(workingPlace.position);
-            //    if (agent.ReachedDestination(workingPlace.position))
-            //    {
-            //        currentState = State.COLLECTING_RESOURCE;
-            //    }
-            //    break;
-            //case State.MOVING_START:
-            //    agent.SetTargetDestination(workingPlace.position);
-            //    currentState = State.MOVING_WORKPLACE;
-            //    break;
-                //case State.MOVING_WAREHOUSE:
-                //    if (agent.ReachedDestination(warehouse.position))
-                //    {
-                //        currentState = State.WAITING;
-                //    }
-                //    break;
-
-                //case State.WAITING:
-                //    if (agent.IsMoving())
-                //        agent.StopAgent();
-                //    break;
-        //}
-    }
-
     public ResourceManager.Resource GetProfession()
     {
         return profession;
