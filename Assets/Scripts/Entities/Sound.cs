308ba2b15b201f30e028ebe80e78549478bce954
pfinnn <finn.isengardt@googlemail.com>
2020-08-30 02:33:10 +0200

    Added AudioManager and 2 Soundtracks

---
+++ Assets/Scripts/Entities/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+
+    public AudioClip clip;
+
+    public bool loop;
+
+    [Range(0f,1f)]
+    public float volume;
+
+    [HideInInspector]
+    public AudioSource source;
+}
