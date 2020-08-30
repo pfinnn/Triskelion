5be537bfe5e9e7e5c0c68ea05834e3578efc1089
pfinnn <finn.isengardt@googlemail.com>
2020-08-30 04:12:12 +0200

    Farmyard Ambients

--- Assets/Scripts/Game Managers/AudioManager.cs
+++ Assets/Scripts/Game Managers/AudioManager.cs
@@ -8,7 +8,8 @@ public class AudioManager : MonoBehaviour
     public Sound[] clips_music;
     public Sound[] clips_UI;
     public Sound[] clips_effects;
-    public Sound[] clips_ambient;
+    public Sound[] clips_ambient_fields;
+    public Sound[] clips_ambient_city;
 
     public AudioSource ambient_City;
     public AudioSource ambient_Field;
@@ -37,6 +38,14 @@ public class AudioManager : MonoBehaviour
     public void Start()
     {
         music.clip = clips_music[0].clip;
+        music.Play();
+
+        ambient_City.clip = clips_ambient_city[0].clip;
+        ambient_City.Play();
+
+        ambient_Field.clip = clips_ambient_fields[0].clip;
+        ambient_Field.Play();
+
     }
 
 
